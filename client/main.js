df5800ac16d827e56cdac1717498afc7a944de24
semigradsky <semigradskyd@gmail.com>
2015-04-30 22:22:36 +0300

    Added removing words

--- client/main.js
+++ client/main.js
@@ -1,13 +1,46 @@
 Template.input.events({
 	'submit .new-sentence': function submitSentence(e) {
 		e.preventDefault();
+
+		var previousSentence = Sentence.findOne({ active: true });
+		
+		if (previousSentence) {
+			var previousSentenceId = previousSentence._id;
+
+			var wordsFromPreviousSentence = Words.find({
+				active: true,
+				sentenceId: previousSentenceId
+			}).fetch();
+			_.each(wordsFromPreviousSentence, function(word) {
+				Words.remove(word._id);
+			});
+			
+			var previousInbox = Inbox.find({
+				active: true,
+				sentenceId: previousSentenceId
+			}).fetch();
+			_.each(previousInbox, function(inbox) {
+				Inbox.remove(inbox._id);
+			});
+	
+			Sentences.update(previousSentenceId, { $set: { active: false } });
+		}
+
 		var text = e.target.text.value;
-		Sentences.insert({ name: text, createdBy: 'me' /*Meteor.userId()*/, active: true });
+		var sentenceId = Sentences.insert({
+			name: text,
+			createdBy: 'me' /*Meteor.userId()*/,
+			active: true
+		});
 		e.target.text.value = '';
 		
 		var words = text.split(' ');
 		_.each(words, function(word) {
-			Words.insert({ name: word, active: true });
+			Words.insert({
+				name: word,
+				active: true,
+				sentenceId: sentenceId
+			});
 		});
 	}
 });
